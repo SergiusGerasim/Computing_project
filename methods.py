71b25d5b07262bb3b95eb144c8d9d674e42b3d0a
VitalinaNikitina <152029963+VitalinaNikitina@users.noreply.github.com>
2024-05-09 23:48:45 +0700

    Update methods.py

--- methods.py
+++ methods.py
@@ -9,6 +9,17 @@ def euler_method(x_array, y_zero, function, dimension):
         y_array[i] = y_array[i - 1] + step * function(x_array[i - 1], y_array[i - 1])
     return y_array
 
+def euler_method_economy(step, a, b, y_zero, function, dimension, filter=10000):
+    number = int((b - a) / step)
+    y_array = np.empty((number - 1) // filter + 1, dimension)
+    y = y_zero
+    for i in range(number - 1):
+        x = a + step * i
+        y = y + step * function(x, y)
+        if i % filter == 0:
+            y_array[i // filter] = y
+    return y_array
+
 def euler_method_recalculation(x_array, y_zero, function, dimension):
     step = x_array[1] - x_array[0]
     size = x_array.shape[0]
@@ -17,6 +28,17 @@ def euler_method_recalculation(x_array, y_zero, function, dimension):
     for i in range(1, size):
         y_array[i] = y_array[i - 1] + step * function(x_array[i - 1], y_array[i - 1])
         y_array[i] = y_array[i - 1] + step * (function(x_array[i - 1], y_array[i - 1]) + function(x_array[i], y_array[i])) / 2
+    return y_array
+
+def euler_method_recalculation_economy(step, a, b, y_zero, function, dimension, filter=10000):
+    number = int((b - a) / step)
+    y_array = np.empty((number - 1) // filter + 1, dimension)
+    y = y_zero
+    for i in range(number - 1):
+        x = a + step * i
+        y = y + step * (function(x, y) + function(x + step, y + step * function(x, y))) / 2
+        if i % filter == 0:
+            y_array[i // filter] = y
     return y_array
 
 def runge_kutta_method(x_array, y_zero, function, dimension):
@@ -31,6 +53,21 @@ def runge_kutta_method(x_array, y_zero, function, dimension):
         fi_4 = step * function(x_array[i - 1] + step, y_array[i - 1] + fi_3)
         y_array[i] = y_array[i - 1] + (fi_1 + 2 * fi_2 + 2 * fi_3 + fi_4) / 6
     return y_array
+
+def runge_kutta_method_economy(step, a, b, y_zero, function, dimension, filter=1):
+    number = int((b - a) / step)
+    y_array = np.empty((number // filter + 1, dimension))
+    y = y_zero
+    for i in range(number):
+        x = a + step * i
+        fi_1 = step * function(x, y)
+        fi_2 = step * function(x + step / 2, y + fi_1 / 2)
+        fi_3 = step * function(x + step / 2, y + fi_2 / 2)
+        fi_4 = step * function(x + step, y + fi_3)
+        y = y + (fi_1 + 2 * fi_2 + 2 * fi_3 + fi_4) / 6
+        if i % filter == 0:
+            y_array[i // filter] = y
+    return y_array
     
 def adams_bashfort_method(x_array, y_zero, function, dimension):
     step = x_array[1] - x_array[0]
@@ -44,6 +81,24 @@ def adams_bashfort_method(x_array, y_zero, function, dimension):
         y_array[i] = y_array[i - 1] + (step / 24) * (55 * f_array[3] - 59 * f_array[2] + 37 * f_array[1] - 9 * f_array[0])
         f_array.pop(0)
         f_array.append(function(x_array[i], y_array[i]))
+    return y_array
+
+def adams_bashfort_method_economy(step, a, b, y_zero, function, dimension, filter= 1):
+    number = int((b - a) / step)
+    y_array = np.empty((number // filter + 1, dimension))
+    y_start = runge_kutta_method(np.array([a, a + step, a + step * 2, a + step * 3]), y_zero, function, dimension)
+    f_array = [function(a, y_start[0]), function(a + step, y_start[1]), 
+               function(a + 2 * step, y_start[2]), function(a + 3 * step, y_start[3])]
+    y = y_start[-1]
+    k = 0
+    for i in range(4, number):
+        x = a + step * i
+        y = y + (step / 24) * (55 * f_array[(k + 3) % 4] - 59 * f_array[(k + 2) % 4]
+                                                      + 37 * f_array[(k + 1) % 4] - 9 * f_array[k])
+        f_array[k] = function(x, y)
+        k = (k + 1) % 4
+        if i % filter == 0:
+            y_array[i // filter] = y
     return y_array
 
 def adams_bashfort_molton_method(x_array, y_zero, function, dimension):
@@ -62,6 +117,25 @@ def adams_bashfort_molton_method(x_array, y_zero, function, dimension):
         f_array[3] = function(x_array[i], y_array[i])
     return y_array
 
+def adams_bashfort_molton_method_economy(step, a, b, y_zero, function, dimension, filter= 1):
+    number = int((b - a) / step)
+    y_array = np.empty((number // filter + 1, dimension))
+    y_start = runge_kutta_method(np.array([a, a + step, a + step * 2, a + step * 3]), y_zero, function, dimension)
+    f_array = [function(a, y_start[0]), function(a + step, y_start[1]), 
+               function(a + 2 * step, y_start[2]), function(a + 3 * step, y_start[3])]
+    y = y_start[-1]
+    k = 0
+    for i in range(4, number):
+        x = a + step * i
+        y = y + (step / 24) * (55 * f_array[(k + 3) % 4] - 59 * f_array[(k + 2) % 4] + 37 * f_array[(k + 1) % 4] - 9 * f_array[k])
+        f_array[k] = function(x, y)
+        k = (k + 1) % 4
+        y = y + (step / 24) * (9 * f_array[(k + 3) % 4] + 19 * f_array[(k + 2) % 4] - 5 * f_array[(k + 1) % 4]  + f_array[k])
+        f_array[(k + 3) % 4] = function(x, y)
+        if i % filter == 0:
+            y_array[i // filter] = y
+    return y_array
+
 def gear_method(x_array, y_zero, function, dimension):
     step = x_array[1] - x_array[0]
     size = x_array.shape[0]
@@ -76,3 +150,18 @@ def gear_method(x_array, y_zero, function, dimension):
             y_iter = y_array[i]
     return y_array
 
+def gear_method_economy(step, a, b, y_zero, function, dimension, filter= 1, iterations= 4):
+    number = int((b - a) / step)
+    y_array = np.empty((number // filter + 1, dimension))
+    y = runge_kutta_method(np.array([a, a + step, a + step * 2, a + step * 3]), y_zero, function, dimension)
+    k = 0
+    for i in range(4, number):
+        y_iter = y[(k + 3) % 4]
+        for _ in range(iterations):
+            y_iter = (48 * y[(k + 3) % 4] - 36 * y[(k + 2) % 4] + 16 * y[(k + 1) % 4]
+                           - 3 * y[k] + 12 * step * function(a + step * i, y_iter)) / 25
+        y[k] = y_iter
+        if i % filter == 0:
+            y_array[i // filter] = y[k]
+        k = (k + 1) % 4
+    return y_array
